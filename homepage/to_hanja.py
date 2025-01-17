23dd931865e3d4b5f8601ec67ab3ae1e935ede80
seolinjung <sj3182@nyu.edu>
2025-01-17 00:16:26 -0800

    FINALLY WORKS

---
+++ homepage/to_hanja.py
@@ -0,0 +1,45 @@
+import os
+import requests
+import krdict
+import urllib.request
+import json
+
+# from bs4 import BeautifulSoup
+
+from django.conf import settings
+
+from konlpy.tag import Kkma
+from konlpy.utils import pprint
+
+
+def convert_word(word):
+    
+    krdict.set_key(settings.API_KEY)
+    response = krdict.search(
+        query=word,
+        raise_api_errors=False
+    )
+
+    try:
+        return response.asdict()["data"]["results"][0]["origin"]
+    except Exception as e:
+        return word
+    
+
+def convert_sentence(sentence):
+
+    kkma = Kkma()
+
+    broken_sentence = kkma.pos(sentence)
+    print("broken sentence is", broken_sentence)
+    new_sentence = []
+
+    for word in broken_sentence:
+        word_to_append = word[0]
+        if (word[1][0] == 'N'):
+        # print("this word is", word[0])
+        # print("attempted to convert to", convert_word(word[0]))
+            word_to_append = convert_word(word[0])
+        new_sentence.append(word_to_append)
+    return " ".join(new_sentence)
+
--- homepage/views.py
+++ homepage/views.py
@@ -4,14 +4,17 @@ from django.http import HttpResponse
 
 from .forms import InputForm
 
+import homepage.to_hanja as to_hanja 
+
 # Create your views here.
 def index(request):
     if request.method == "POST":
         form = InputForm(request.POST)
         if form.is_valid():
-            form_data = form.save()
-            
-            return render(request, 'homepage/result.html', {"result": form_data})
+            input = form.save()
+            # print(type(form.text))
+            output = to_hanja.convert_sentence(input.text)
+            return render(request, 'homepage/result.html', {"result": output})
             # return redirect('result')
     else:
         form = InputForm()
